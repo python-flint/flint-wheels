f6d12a1e90a898ed54f08b0a45c9dd7997270b5a
xoviat <xoviat@users.noreply.github.com>
2017-10-09 12:25:08 -0500

    Finalize initial configuration

---
+++ config.sh
@@ -0,0 +1,27 @@
+# Define custom utilities
+# Test for OSX with [ -n "$IS_OSX" ]
+
+function pre_build {
+    # Any stuff that you need to do before you start building the wheels
+    # Runs in the root directory of this repository.
+    cd arb
+    ./.build_dependencies
+    ./configure --with-mpir=$HOME/deps --with-mpfr=$HOME/deps --with-flint=$HOME/deps --prefix=$HOME/deps
+    make -j4
+    make install
+    cd ..
+
+    # Now go into the project directory and run a custom build_ext command
+    cd python-flint
+    python -m pip install cython
+    python setup.py build_ext \
+        --include-dirs=/root/deps/include:/root/deps/include/flint \
+        --library-dirs=/root/deps/lib:/root/deps/lib/flint
+    cd ..
+    export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/root/deps/lib:/root/deps/lib/flint
+}
+
+function run_tests {
+    # Runs tests on installed distribution from an empty directory
+    :
+}
