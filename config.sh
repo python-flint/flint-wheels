00b596027b688404e0c8f73ca98c7b5652917a9f
xoviat <xoviat@users.noreply.github.com>
2017-10-09 15:41:17 -0500

    config: restore custom build_ext

--- config.sh
+++ config.sh
@@ -5,6 +5,11 @@ function pre_build {
     # Any stuff that you need to do before you start building the wheels
     # Runs in the root directory of this repository.
     source ./build_dependencies.sh
+    
+    # Now go into the project directory and run a custom build_ext command
+    cd python-flint
+    python setup.py build_ext --include-dirs=$LD_INCLUDE_PATH --library-dirs=$LD_LIBRARY_PATH
+    cd ..
 }
 
 function run_tests {
