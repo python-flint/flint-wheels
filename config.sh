2b10c6997fc49d2d8ebf9fc0ffd7bd3a15912f0a
xoviat <xoviat@users.noreply.github.com>
2017-10-09 15:10:41 -0500

    config: remove custom build_ext

--- config.sh
+++ config.sh
@@ -5,15 +5,6 @@ function pre_build {
     # Any stuff that you need to do before you start building the wheels
     # Runs in the root directory of this repository.
     source ./build_dependencies.sh
-
-    # Now go into the project directory and run a custom build_ext command
-    cd python-flint
-    python -m pip install cython
-    python setup.py build_ext \
-        --include-dirs=/root/deps/include:/root/deps/include/flint \
-        --library-dirs=/root/deps/lib:/root/deps/lib/flint
-    cd ..
-    export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/root/deps/lib:/root/deps/lib/flint
 }
 
 function run_tests {
