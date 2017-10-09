13f0c0694d36bcd41a56e4d4e44d438f6cf7464e
xoviat <xoviat@users.noreply.github.com>
2017-10-09 14:32:08 -0500

    build_dependencies: test for osx

--- build_dependencies.sh
+++ build_dependencies.sh
@@ -1,12 +1,22 @@
 #! /bin/sh
 
-wget http://mpir.org/mpir-2.7.0.tar.bz2
-tar -xf mpir-2.7.0.tar.bz2
-cd mpir-2.7.0
-./configure --enable-gmpcompat --prefix=$HOME/deps --disable-static --enable-fat
-make -j4 > /dev/null 2>&1
-make install
-cd ..
+if [ -n $IS_OSX ]; then
+    wget http://mpir.org/mpir-2.7.0.tar.bz2
+    tar -xf mpir-2.7.0.tar.bz2
+    cd mpir-2.7.0
+    ./configure --enable-gmpcompat --prefix=$HOME/deps --disable-static --host=x86_64-darwin-none
+    make -j4 > /dev/null 2>&1
+    make install
+    cd ..
+else
+    wget http://mpir.org/mpir-2.7.0.tar.bz2
+    tar -xf mpir-2.7.0.tar.bz2
+    cd mpir-2.7.0
+    ./configure --enable-gmpcompat --prefix=$HOME/deps --disable-static --enable-fat
+    make -j4 > /dev/null 2>&1
+    make install
+    cd ..
+fi
 
 wget http://www.mpfr.org/mpfr-3.1.4/mpfr-3.1.4.tar.bz2
 tar -xf mpfr-3.1.4.tar.bz2
