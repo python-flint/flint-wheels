d0e9c4c9854186b0c8fa2066e8eba2947f8d973e
xoviat <xoviat@users.noreply.github.com>
2017-10-17 22:51:56 -0500

    BLD: remove the custom build command

--- config.sh
+++ config.sh
@@ -5,12 +5,6 @@ function pre_build {
     # Any stuff that you need to do before you start building the wheels
     # Runs in the root directory of this repository.
     source ./build_dependencies.sh
-    
-    # Now go into the project directory and run a custom build_ext command
-    cd python-flint
-    python -m pip install cython
-    python setup.py build_ext --include-dirs=$LD_INCLUDE_PATH --library-dirs=$LD_LIBRARY_PATH
-    cd ..
 }
 
 function run_tests {
