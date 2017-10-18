e8879f6448beddb263b7201be8c04b7ec783f944
xoviat <xoviat@users.noreply.github.com>
2017-10-17 21:17:56 -0500

    FIX: update external symbol

--- printf.c
+++ printf.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdarg.h>
 
-extern int _gmpfr_fprintf(const char *fmt, va_list argp) {
+extern int __gmpfr_fprintf(const char *fmt, va_list argp) {
     return fprintf(stderr, fmt, argp);
 }
