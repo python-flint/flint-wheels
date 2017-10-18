7b4ef267f234c577eb85a11fffdca02909d84f45
xoviat <xoviat@users.noreply.github.com>
2017-10-18 13:28:27 -0500

    BLD: Manylinux: cleanup dependency installation (#8)
    * BLD: remove dependency on arb submodule
    
    * BLD: use build_simple to build some libraries
    
    * FIX: untar the correct file
    
    * BLD: use build_github function to build arb
    
    * FIX: repair build_github
    
    * FIX: use the correct archive path
    
    * FIX: use the correct archive basename
    
    * FIX: change to the correct directory
    
    * BLD: move build_github
    
    * FIX: use name
    
    * MAINT: move build_dependencies into config

--- build_dependencies.sh
+++ build_dependencies.sh
@@ -2,11 +2,29 @@
 
 set -e -x
 
+function build_github {
+    local path=$1
+    local version=$2
+    local configure_args=${@:3}
+    if [ -e "${name}-stamp" ]; then
+        return
+    fi
+    local name=`basename "$path"`
+    local name_version="${name}-${version}"
+    fetch_unpack "https://github.com/${path}/archive/${version}.tar.gz"
+    ls -r .
+    (cd $name_version \
+        && ./configure --prefix=$BUILD_PREFIX $configure_args \
+        && make \
+        && make install)
+    touch "${name}-stamp"
+}
+
 if [ -n "$IS_OSX" ]; then
     HOMEBREW_NO_AUTO_UPDATE=1 brew install homebrew/science/arb
 else
     local PLATFORM_ARGS=--enable-fat
-    
+
     wget http://mpir.org/mpir-2.7.0.tar.bz2
     tar -xf mpir-2.7.0.tar.bz2
     cd mpir-2.7.0
@@ -15,28 +33,9 @@ else
     make install
     cd ..
 
-    wget http://www.mpfr.org/mpfr-3.1.4/mpfr-3.1.4.tar.bz2
-    tar -xf mpfr-3.1.4.tar.bz2
-    cd mpfr-3.1.4
-    ./configure --prefix=$BUILD_PREFIX --disable-static $PLATFORM_ARGS
-    make -j4 > /dev/null 2>&1
-    make install
-    cd ..
-
-    wget http://flintlib.org/flint-2.5.2.tar.gz
-    tar -xf flint-2.5.2.tar.gz
-    cd flint-2.5.2
-    ./configure --prefix=$BUILD_PREFIX --disable-static
-    make -j4 > /dev/null 2>&1
-    make install
-    cd ..
-
-    cd arb
-    ./configure --prefix=$BUILD_PREFIX
-    make -j4
-    make install
-    cd ..
-    
+    build_simple mpfr 3.1.4 http://www.mpfr.org/mpfr-3.1.4/
+    build_simple flint 2.5.2 http://flintlib.org/
+    build_github fredrik-johansson/arb 2.11.1
 fi
 
 # Copy the flint headers
--- config.sh
+++ config.sh
@@ -1,10 +1,46 @@
 # Define custom utilities
 # Test for OSX with [ -n "$IS_OSX" ]
 
+function build_github {
+    # Example: build_github fredrik-johansson/arb 2.11.1
+    local path=$1
+    local version=$2
+    local configure_args=${@:3}
+    local name=`basename "$path"`
+    local name_version="${name}-${version}"
+    if [ -e "${name}-stamp" ]; then
+        return
+    fi
+    fetch_unpack "https://github.com/${path}/archive/${version}.tar.gz"
+    ls -r .
+    (cd $name_version \
+        && ./configure --prefix=$BUILD_PREFIX $configure_args \
+        && make \
+        && make install)
+    touch "${name}-stamp"
+}
+
 function pre_build {
     # Any stuff that you need to do before you start building the wheels
     # Runs in the root directory of this repository.
-    source ./build_dependencies.sh
+    if [ -n "$IS_OSX" ]; then
+        HOMEBREW_NO_AUTO_UPDATE=1 brew install homebrew/science/arb
+    else
+        wget http://mpir.org/mpir-2.7.0.tar.bz2
+        tar -xf mpir-2.7.0.tar.bz2
+        cd mpir-2.7.0
+        ./configure --enable-gmpcompat --prefix=$BUILD_PREFIX --disable-static --enable-fat
+        make -j4 > /dev/null 2>&1
+        make install
+        cd ..
+
+        build_simple mpfr 3.1.4 http://www.mpfr.org/mpfr-3.1.4/
+        build_simple flint 2.5.2 http://flintlib.org/
+        build_github fredrik-johansson/arb 2.11.1
+    fi
+
+    # Copy the flint headers
+    cp /usr/local/include/flint/*.h /usr/local/include
 }
 
 function run_tests {
